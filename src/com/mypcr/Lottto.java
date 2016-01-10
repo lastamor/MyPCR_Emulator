5f8a3460bca2094fd11c4a0fe58a5c89c94f7221
Your Name <you@example.com>
2016-01-11 00:38:16 +0900

    commit Lotto tast

--- src/com/mypcr/Lottto.java
+++ src/com/mypcr/Lottto.java
@@ -2,18 +2,19 @@ package com.mypcr;
 
 public class Lottto {
 
-	
-	public int [] lotto_print()
+	//print
+	public int [] lotto_generation()
 	{
-		int []a5 = new int[7];
-		for (int i = 0; i < a5.length; i++) {
+		int []a = new int[7];
+		
+		for (int i = 0; i < a.length; i++) {
 			int res = (int)(Math.random()*45)+1;
 			
-			a5[i] = res;
+			a[i] = res;
 			
 			for (int j = 0; j < i; j++) {
-				if (res  == a5[j]) {
-					System.out.println("숫자가 중복 되었습니다.다른 숫자를 입력하세요");
+				if (res  == a[j]) {
+					//System.out.println("숫자가 중복 되었습니다.다른 숫자를 입력하세요");
 					i--;
 					break;
 				}
@@ -21,8 +22,46 @@ public class Lottto {
 			
 			
 		}
-		return a5;
+		return a;
 	}
+	
+	
+	public void number_comparison( int []a1, int []a2)
+	{
+		int res=0;
+		
+		for (int i = 0; i < a1.length; i++) {
+			for (int j = 0; j < a1.length; j++) {
+				if(a1[i] == a2[j]){
+					res++;
+					break;
+				}
+			}
+			
+		}
+		System.out.print("등수: ");
+		if (res != 0) {
+			String str = new String(8-res + "등 (" + res +"개 맞품)");
+			System.out.println(str);
+		} else {
+			System.out.println("- 꽝 -");
+		}
+		
+	}
+	
+	public void lotto_print (int []a0) {
+
+		for (int i=0; i<a0.length; i++)
+		{
+			if (i == a0.length -1 ) {
+				System.out.println(a0[i]);
+			} else {
+				System.out.print(a0[i] + ", ");
+			}
+		}
+	}
+	
+
 	
 }
 
--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -3,6 +3,7 @@ package com.mypcr;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 
 import com.mypcr.emulator.MyPCR;
 
@@ -13,25 +14,68 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a1 = 25;
-		int a2 = 26;
-		int a3 = 27;
-		int a4 = 29;
-						
-		Lottto lotto = new Lottto();
-		
-		
-		int []a5 =  lotto.lotto_print();
-		
-		for (int i : a5)
-			System.out.println(i);
 
-		//System.out.println((int)(Math.random()*45)+1);
+		Lottto lotto = new Lottto();	
+		
+		int []a0 =  lotto.lotto_generation();
+		
+		int []a1 = lotto.lotto_generation();
+		
+		int []a2 = lotto.lotto_generation();
+		
+		int []a3 = lotto.lotto_generation();
 
+		quickSort(a0, 0, a0.length-1);
+		quickSort(a1, 0, a0.length-1);
+		quickSort(a2, 0, a0.length-1);
+		quickSort(a3, 0, a0.length-1);
+		
+		System.out.println("** ´ç Ã· ¹ø È£ **");
+		lotto.lotto_print(a0);
+		
+		System.out.println("1st LOTTO Number");
+		lotto.lotto_print(a1);
+		lotto.number_comparison(a0, a1);
+	
+		System.out.println("2st LOTTO Number");
+		lotto.lotto_print(a2);
+		lotto.number_comparison(a0, a2);
+		
+		System.out.println("3st LOTTO Number");
+		lotto.lotto_print(a3);
+		lotto.number_comparison(a0, a3);
 		
 	}
-
-		
-		
+	
+	public static void quickSort(int arr[], int left, int right) {
+	      int index = partition(arr, left, right);
+	      if (left < index - 1)
+	            quickSort(arr, left, index - 1);
+	      if (index < right)
+	            quickSort(arr, index, right);
+	}
+	
+	static int partition(int arr[], int left, int right)
+	{
+	      int i = left, j = right;
+	      int tmp;
+	      int pivot = arr[(left + right) / 2];
+	     
+	      while (i <= j) {
+	            while (arr[i] < pivot)
+	                  i++;
+	            while (arr[j] > pivot)
+	                  j--;
+	            if (i <= j) {
+	                  tmp = arr[i];
+	                  arr[i] = arr[j];
+	                  arr[j] = tmp;
+	                  i++;
+	                  j--;
+	            }
+	      };
+	     
+	      return i;
+	}
 
 }
