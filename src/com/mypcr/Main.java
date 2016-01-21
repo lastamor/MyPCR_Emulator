1a5de360143481a838e2ebf4893cea6660b133d1
lastamor@gmail.com <lastamor@gmail.com>
2016-01-21 21:03:53 +0900

    commit Protocol_lis_split 과제

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -20,11 +20,9 @@ public class Main {
 			String[] temp2 = temp.split("\t");
 			
 		//	System.out.println( temp2[0] + "," + temp2[1] + "," +temp2[2]);
-			Protocol p1 = new Protocol( temp2[0], Integer.parseInt(temp2[1]),  Integer.parseInt(temp2[2]));
-			list.add(p1);
-			
+			Protocol p = new Protocol( temp2[0], Integer.parseInt(temp2[1]),  Integer.parseInt(temp2[2]));
+			list.add(p);
 		}		
-
 		
 		System.out.println("==== Protocol List ====");
 		System.out.println("Label"+"\t"+ "Temp" + "\t" + "Time");
@@ -36,3 +34,30 @@ public class Main {
 		}
 	}
 }
+
+/*
+ * 
+ 
+	String pcr1 = "1	95	10\n" +
+				  "2	kk	30\n" + 
+				  "3	72	30\n";
+				  
+ 	String pcr1 = "1	95	10\n" +
+				  "2	60	30\n" + 
+				  "3	72\n";
+				  
+	1. MyPCR에 다음과 같은 하수를 만드세요.
+		public ArrayList<Protocol> makeProtocolList(String pcr)
+		위의 함수는 이전에 구현한 pcr line 값을 읽어서 pcr list를 만드는 예제를 이용해야 합니다.
+		(파일 읽기 부분 구현)
+		매개 변수 pcr은 위의 pcr1, pcr2 를 사용하면 됩니다.
+		
+	2. (1) 번 예제를 돌리 시, 에러가 발생합니다.
+		에레 중에 NumberFormatExeption 또는 ArrayOutofIndexBound 에러가 발생할 경우,
+		"잘못된 PCR 파일입니다" 라고 출력 후에 return null; 을 호출해 주세요.
+		
+	3.	public void showProtocolList(ArrayList<Protocol> list);
+		위의 함수는 이전에 구현한 프로토콜 출력 부분을 이용하면 됩니다.
+		단 매개변수로 받은 list가 null 인지 체크하고, null 일 경우, "잘목된 PCR 파일 출력하려 했습니다."
+		라고 출력 후에 return;을 호출해주세요.
+ */ 
