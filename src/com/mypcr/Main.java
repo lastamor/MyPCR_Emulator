04f1a7a889d65bae23be133f283a169b18e2b88c
lastamor@gmail.com <lastamor@gmail.com>
2016-01-21 20:52:32 +0900

    commit Protocol_lis_split

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -8,16 +8,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String pcr = "1	95	10\n" +
+					 "2	60	30\n" + 
+					 "3	72	30\n";
+		String[] pcrs = pcr.split("\n");
 		ArrayList<Protocol> list = new ArrayList<Protocol>();
-		Protocol p1 = new Protocol("1", 20, 20);
-		list.add(p1);
+
+		for (int i = 0; i < pcrs.length; i++) {
+			String temp = pcrs[i];
+			String[] temp2 = temp.split("\t");
+			
+		//	System.out.println( temp2[0] + "," + temp2[1] + "," +temp2[2]);
+			Protocol p1 = new Protocol( temp2[0], Integer.parseInt(temp2[1]),  Integer.parseInt(temp2[2]));
+			list.add(p1);
+			
+		}		
+
 		
 		System.out.println("==== Protocol List ====");
 		System.out.println("Label"+"\t"+ "Temp" + "\t" + "Time");
 		for (int i = 0; i < list.size(); i++) {
 			Protocol p = list.get(i);
-			System.out.println( p.getLabel() + "\t" +
-			                    p.getTemp() + "\t"+ 
+			System.out.println( p.getLabel() + "\t"+
+			                    p.getTemp()  + "\t"+ 
 								p.getTime());	
 		}
 	}
