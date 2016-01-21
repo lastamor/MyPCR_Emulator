e481f4eda90a41392f853d67e034d1b1c6452cc3
lastamor@gmail.com <lastamor@gmail.com>
2016-01-21 20:20:37 +0900

    commit Protocol

--- src/com/mypcr/Lottto.java
+++ src/com/mypcr/Lottto.java
@@ -19,8 +19,6 @@ public class Lottto {
 					break;
 				}
 			}
-			
-			
 		}
 		return a;
 	}
--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -1,81 +1,24 @@
 package com.mypcr;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
+import java.util.ArrayList;
 
-import com.mypcr.emulator.MyPCR;
+import com.mypcr.emulator.Protocol;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Lottto lotto = new Lottto();	
+		ArrayList<Protocol> list = new ArrayList<Protocol>();
+		Protocol p1 = new Protocol("1", 20, 20);
+		list.add(p1);
 		
-		int []a0 =  lotto.lotto_generation();
-		
-		int []a1 = lotto.lotto_generation();
-		
-		int []a2 = lotto.lotto_generation();
-		
-		int []a3 = lotto.lotto_generation();
-
-		quickSort(a0, 0, a0.length-1);
-		quickSort(a1, 0, a0.length-1);
-		quickSort(a2, 0, a0.length-1);
-		quickSort(a3, 0, a0.length-1);
-		
-		System.out.println("** ´ç Ã· ¹ø È£ **");
-		lotto.lotto_print(a0);
-		
-		System.out.println("1st LOTTO Number");
-		lotto.lotto_print(a1);
-		lotto.number_comparison(a0, a1);
-	
-		System.out.println("2st LOTTO Number");
-		lotto.lotto_print(a2);
-		lotto.number_comparison(a0, a2);
-		
-		System.out.println("3st LOTTO Number");
-		lotto.lotto_print(a3);
-		lotto.number_comparison(a0, a3);
-		
+		System.out.println("==== Protocol List ====");
+		System.out.println("Label"+"\t"+ "Temp" + "\t" + "Time");
+		for (int i = 0; i < list.size(); i++) {
+			Protocol p = list.get(i);
+			System.out.println( p.getLabel() + "\t" +
+			                    p.getTemp() + "\t"+ 
+								p.getTime());	
+		}
 	}
-	
-	public static void quickSort(int arr[], int left, int right) {
-	      int index = partition(arr, left, right);
-	      if (left < index - 1)
-	            quickSort(arr, left, index - 1);
-	      if (index < right)
-	            quickSort(arr, index, right);
-	}
-	
-	static int partition(int arr[], int left, int right)
-	{
-	      int i = left, j = right;
-	      int tmp;
-	      int pivot = arr[(left + right) / 2];
-	     
-	      while (i <= j) {
-	            while (arr[i] < pivot)
-	                  i++;
-	            while (arr[j] > pivot)
-	                  j--;
-	            if (i <= j) {
-	                  tmp = arr[i];
-	                  arr[i] = arr[j];
-	                  arr[j] = tmp;
-	                  i++;
-	                  j--;
-	            }
-	      };
-	     
-	      return i;
-	}
-
 }
