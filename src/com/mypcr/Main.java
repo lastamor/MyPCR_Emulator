00a4f39baded64df8b2c56a1d1bef05c74eac94a
lastamor@gmail.com <lastamor@gmail.com>
2016-02-04 20:53:44 +0900

    commit 과제

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -16,114 +16,63 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		//BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader in = null;
-		ArrayList<Protocol> list = new ArrayList<Protocol>();		
+		final MyPCR pcr = new MyPCR();
+		pcr.start();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		try {
-			in = new BufferedReader(new FileReader(new File("procotol.txt")));
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
 		
-	
-		try{
-		
-			String line = null;
-			String line2 = "";
-			while ((line = in.readLine()) != null) {
-				
-				line2 = line2 + line + "\n";
-			}
-			in.close();
-		
+		while (true) {
 
-			list = MyPCR.makeProtocolList(line2);
-		
-			
-			for (int i = 0; i < list.size(); i++) {
-				Protocol protocol = list.get(i);
-				
-				if(protocol.getLabel().equals("GOTO")){
-					
-					int target = protocol.getTemp();
-					int time = protocol.getTime();
-					protocol.setTime(time-1);
-					
-					if(time-1 != 0){
-						i= target-2;
-					}
+			try {
+				String cmd = in.readLine();
+
+						
+				if(cmd.equals("start")){
+					pcr.startPCR();
 				}
-				else{
-					System.out.println( protocol.getLabel() + "\t"+
-							protocol.getTemp()  + "\t"+ 
-							protocol.getTime());	
+				else if(cmd.equals("stop")){
+					pcr.stopPCR();
+				}
+					
+				else if(cmd.equals("print")){
+					pcr.printStatus();
+				}
+				else if(cmd.equals("monitor")){		
+					pcr.setMonitoring(true);
+					Thread t = new Thread(){
+						public void run(){
+							while(pcr.isMonitoring()){
+								try {
+									Thread.sleep(100);
+									pcr.printStatus();
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							}
+						}
+					};
+					t.start();
+					in.readLine();
+					pcr.setMonitoring(false);
 				}
 				
+			} catch (IOException e) {
+				e.printStackTrace();
+
 			}
-		
-			
-			MyPCR p = new MyPCR();
-			
-			
-			
-		}catch(IOException e){
-			e.printStackTrace();
+
 		}
-		
-
 
 		
-		/*		
 		
-		ArrayList<Protocol> list = new ArrayList<Protocol>();
+		/*
+		String data = pcr.loadProtocolFromFile("protocol.txt");
+		
+		ArrayList<Protocol> list = p.makeProtocolList(data);
+		
+		p.printProtocolList(list);
 
-		for (int i = 0; i < pcr_0.length; i++) {
-			String temp = pcr_0[i];
-			String[] temp2 = temp.split("\t");
-			
-		//	System.out.println( temp2[0] + "," + temp2[1] + "," +temp2[2]);
-			Protocol p = new Protocol( temp2[0], Integer.parseInt(temp2[1]),  Integer.parseInt(temp2[2]));
-			list.add(p);
-		}		
-		
-		System.out.println("==== Protocol List ====");
-		System.out.println("Label"+"\t"+ "Temp" + "\t" + "Time");
-		for (int i = 0; i < list.size(); i++) {
-			Protocol p = list.get(i);
-			System.out.println( p.getLabel() + "\t"+
-			                    p.getTemp()  + "\t"+ 
-								p.getTime());	
-		}
-		
 		*/
-		
 	}
 }
-
-/*
- * 
- 
-	String pcr1 = "1	95	10\n" +
-				  "2	kk	30\n" + 
-				  "3	72	30\n";
-				  
- 	String pcr1 = "1	95	10\n" +
-				  "2	60	30\n" + 
-				  "3	72\n";
-				  
-	1. MyPCR에 다음과 같은 하수를 만드세요.
-		public ArrayList<Protocol> makeProtocolList(String pcr)
-		위의 함수는 이전에 구현한 pcr line 값을 읽어서 pcr list를 만드는 예제를 이용해야 합니다.
-		(파일 읽기 부분 구현)
-		매개 변수 pcr은 위의 pcr1, pcr2 를 사용하면 됩니다.
-		
-	2. (1) 번 예제를 돌리 시, 에러가 발생합니다.
-		에레 중에 NumberFormatExeption 또는 ArrayOutofIndexBound 에러가 발생할 경우,
-		"잘못된 PCR 파일입니다" 라고 출력 후에 return null; 을 호출해 주세요.
-		
-	3.	public void showProtocolList(ArrayList<Protocol> list);
-		위의 함수는 이전에 구현한 프로토콜 출력 부분을 이용하면 됩니다.
-		단 매개변수로 받은 list가 null 인지 체크하고, null 일 경우, "잘목된 PCR 파일 출력하려 했습니다."
-		라고 출력 후에 return;을 호출해주세요.
- */ 
--- src/com/mypcr/emulator/MyPCR.java
+++ src/com/mypcr/emulator/MyPCR.java
@@ -1,16 +1,64 @@
 package com.mypcr.emulator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class MyPCR {
+public class MyPCR extends Thread {
 
 	//firmware source 황용하여
 	//
-	public MyPCR(){
+	private double mTemp;
+	private double mPreTemp, mTargetTemp;
+	private int state;
+	
+	private boolean isMonitor = false;
+	
+	private static final int STATE_READY = 0x00;
+	private static final int STATE_RUN = 0x01;
+	
+	private static final double DEFAULT_TEMP = 25.1;
+	
 
+	
+	public MyPCR(){
+		mTemp = 25.1;
+		mPreTemp = 25.1;
+		mTargetTemp = 25.1;
+		state = STATE_READY;
 	}
 	
-	public static ArrayList<Protocol> makeProtocolList(String pcr){
+	public void run(){
+		
+		while(true){
+			//
+			
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if(state == STATE_RUN){
+				
+				mTemp += 0.25;
+				
+			}else{
+			
+				mTemp = DEFAULT_TEMP;
+				mPreTemp = DEFAULT_TEMP;
+				mTargetTemp = DEFAULT_TEMP;
+				
+			}		
+		}
+	}
+	
+	public ArrayList<Protocol> makeProtocolList(String pcr){
+		if(pcr ==null)
+			return null;
 		
 		ArrayList<Protocol> list = new ArrayList<Protocol>();	
 		
@@ -34,7 +82,7 @@ public class MyPCR {
 		return list;
 	}
 	
-	public static void printProtocolList(ArrayList<Protocol> pcr){
+	public  void printProtocolList(ArrayList<Protocol> pcr){
 		System.out.println("==== Protocol List ====");
 		System.out.println("Label"+"\t"+ "Temp" + "\t" + "Time");
 		for (int i = 0; i < pcr.size(); i++) {
@@ -46,7 +94,7 @@ public class MyPCR {
 		
 	}
 
-	public static void printProtocolList2(ArrayList<Protocol> pcr){
+	public  void printProtocolList2(ArrayList<Protocol> pcr){
 		System.out.println("==== Protocol List ====");
 		System.out.println("Label"+"\t"+ "Temp" + "\t" + "Time");
 		for (int i = 0; i < pcr.size(); i++) {
@@ -56,7 +104,89 @@ public class MyPCR {
 								p.getTime());	
 		}
 		
+	}
+	
+	public String loadProtocolFromFile(String path){
+		
+		BufferedReader in = null;
+		String line = null;
+		String line2 = "";
+
+		try {
+			in = new BufferedReader(new FileReader(new File(path)));
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+		
+
+		try{
+			
+			while ((line = in.readLine()) != null) {
+				
+				line2 = line2 + line + "\n";
+			}
+			in.close();
+		
+			
+		}catch(IOException e){
+			e.printStackTrace();
+			return null;
+		}
+		
+		return line2;
+	}
+	
+	private String getStateString(){
+		
+		if (state==STATE_READY)
+			return "준비중";
+		else
+			return "동작중";				
+	}
+	
+	public void printStatus(){
+		System.out.println(String.format("상태: %s,  온도: %3.1f", getStateString(),mTemp));
+	}
+	
+	public void startPCR(){
+		state = STATE_RUN;
+	}
+	
+	public void stopPCR(){
+		state = STATE_READY;
+	}
+	
+	public void setMonitoring(boolean monitor){
+		isMonitor = monitor;
+	}
+	public boolean isMonitoring(){
+		return isMonitor;
 	}
 
 //	private
 }
+
+
+/**
+1. startPCR(), stropPCR() 시, PCR 시작!, PCR 종료!
+2. start 시, mPrevTemp = 95.0, mTargetTemp = 50.0 으로 변경,
+	run() 함수 내에서 mPrevTemp 까지 온도 도달 시키고,
+	그 후에 mTargetTemp 까지 온도가 도달하도록 구현,
+	mTargetTemp 까지 온도가 도달할 경우, 자동으로 Stop 되도록 구현
+	
+3. startPCR(), stopPCR() 시, 현재 상태가 맞지 않은 경우 그냥 무시하도록 구현,
+
+	ex) start를 했는데, state가 이미 RUN 이면 그냥 return  하도록 구현,
+	stop 도 마찬가지로....
+	
+4. private int mElapsedTime 선언 후 , 0으로 초기화,
+	run() 함수에서, state 가 RUN 일 경우, 1sec 마다 증가시킴,
+	printstatus() 함수에서 String.format"상태: %s,  온도: %3.1f", elapsedTime: %s", getStateString(),mTemp, getelapsedTime)); 으로 구현
+	결과물은 다음과 같음: 상태: 동작중, 온도: 78.1, elapsedTime: 1m 2s
+	run() 함수에서의 mTemp += 0.01; 로 변경하세요.
+	getElapsedTime() 함수는 다음과 같이 선언하여 구현하세요.
+	private String getElapsedTime()
+	
+
+**/
