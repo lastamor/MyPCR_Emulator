0997b47e0366656340e5145a1d66341f54f1ca64
lastamor@gmail.com <lastamor@gmail.com>
2016-02-11 20:41:17 +0900

    commit 배열로 온도를 올렸다 내렸다

--- src/com/mypcr/emulator/MyPCR.java
+++ src/com/mypcr/emulator/MyPCR.java
@@ -16,14 +16,20 @@ public class MyPCR extends Thread {
 	private int state;
 	private int mElapsedTime;
 	private int mSecondCount;
+	private int mArrangeCount;
 	private boolean isMonitor = false;
+	private boolean mTempFalg = false;
+	private boolean mTempTargetFalg = false;
 	
 	private static final int STATE_READY = 0x00;
 	private static final int STATE_RUN = 0x01;
 	
 	private static final double DEFAULT_TEMP = 25.1;
 	
+	
+	private static final double temps[] = { 95.5, 72.0, 85.0, 50.0, 4.0}; 
 
+	
 	
 	public MyPCR(){
 		mTemp = 25.1;
@@ -32,31 +38,75 @@ public class MyPCR extends Thread {
 		state = STATE_READY;
 		mElapsedTime = 0;
 		mSecondCount = 0;
+		
+		
 	}
 	
 	public void run(){
 		
 		while(true){
-			//
-			
-			
+	
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			
 			if(state == STATE_RUN){
-				if(mTemp <= mPreTemp){
-					mTemp += 1.0;	
-					}
-				else{
-					if(mTemp <= mTargetTemp)
-						mTemp += 1.0;	
-					else
-						stopPCR();			
+		
+				if( mTargetTemp - mPreTemp< 0){
+					mTemp -= 1.0;
+					if(mTemp < mTargetTemp)
+						mTempTargetFalg = true;
+				}	
+				else {
+					mTemp += 1.0;
+					if(mTemp > mTargetTemp)
+						mTempTargetFalg = true;
 				}
+	
+				if(mTempTargetFalg){
+					mTempTargetFalg = false;
+					mArrangeCount++;
+					mPreTemp = mTargetTemp;
+					if(temps.length <= mArrangeCount){
+						stopPCR();
+					}else{
+						mTargetTemp = temps[mArrangeCount];
+					}
+				}
+				/*
+				if(mTemp > mTargetTemp && !mTempFalg )
+				{
+					mArrangeCount++;
+					mPreTemp = mTargetTemp;
+					if(temps.length < mArrangeCount){
+						stopPCR();
+					}else{
+						mTargetTemp = temps[mArrangeCount];
+					}
+
+				}
+				if(mTemp < mTargetTemp && mTempFalg ){
+					mArrangeCount++;
+					mPreTemp = mTargetTemp;
+					if(temps.length <= mArrangeCount){
+						stopPCR();
+					}else{
+						mTargetTemp = temps[mArrangeCount];
+					}
+				}
+			*/
+					
 				
+				// 1. 배열 읽어 오기
+				// 2. 첫번째 배열 값을 mTargetTemp에 넣기
+				// 3. mPreTemp - mTargetTemp 비교 해서 0보다 크면 1도식 증가&&mTempFlag =false 작으면 1도씩 감소 &&mTempFlag =true
+				// 4. mTargetTemp 도달 후 다음 배열로 이동
+				// 5. mPreTemp을 mTargetTemp에 넣고 mTargetTemp를 다음 온도에 넣는다.
+			
+				
+						
 				mElapsedTime++;	
 				
 		
@@ -173,7 +223,6 @@ public class MyPCR extends Thread {
 		minutes = seconds / 60;
 		seconds = seconds % 60;
 		
-
 		
 		if (houre != 0 ) {
 			str = String.format("%dh %dm %ds",houre,minutes,seconds);
@@ -199,8 +248,9 @@ public class MyPCR extends Thread {
 		if(state == STATE_RUN)
 			return;
 		state = STATE_RUN;
-		mPreTemp = 50;
-		mTargetTemp = 95;
+		mPreTemp = DEFAULT_TEMP;
+		mArrangeCount = 0;
+		mTargetTemp = temps[mArrangeCount];
 		System.out.println("PCR 시작!");
 		
 	}
@@ -224,25 +274,3 @@ public class MyPCR extends Thread {
 }
 
 
-/**
-1. startPCR(), stropPCR() 시, PCR 시작!, PCR 종료!
-2. start 시, mPrevTemp = 95.0, mTargetTemp = 50.0 으로 변경,
-	run() 함수 내에서 mPrevTemp 까지 온도 도달 시키고,
-	그 후에 mTargetTemp 까지 온도가 도달하도록 구현,
-	mTargetTemp 까지 온도가 도달할 경우, 자동으로 Stop 되도록 구현
-	
-3. startPCR(), stopPCR() 시, 현재 상태가 맞지 않은 경우 그냥 무시하도록 구현,
-
-	ex) start를 했는데, state가 이미 RUN 이면 그냥 return  하도록 구현,
-	stop 도 마찬가지로....
-	
-4. private int mElapsedTime 선언 후 , 0으로 초기화,
-	run() 함수에서, state 가 RUN 일 경우, 1sec 마다 증가시킴,
-	printstatus() 함수에서 String.format"상태: %s,  온도: %3.1f", elapsedTime: %s", getStateString(),mTemp, getelapsedTime)); 으로 구현
-	결과물은 다음과 같음: 상태: 동작중, 온도: 78.1, elapsedTime: 1m 2s
-	run() 함수에서의 mTemp += 0.01; 로 변경하세요.
-	getElapsedTime() 함수는 다음과 같이 선언하여 구현하세요.
-	private String getElapsedTime()
-	
-
-**/
